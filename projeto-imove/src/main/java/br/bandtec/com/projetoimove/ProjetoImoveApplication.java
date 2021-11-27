be762e3910be4a960dc5ec1013f188bcc102fff9
Rennan-Aquino <rennan.silva@bandtec.com.br>
2021-11-27 14:45:54 -0300

    teste controller Usuario - teste em get post put

--- projeto-imove/src/main/java/br/bandtec/com/projetoimove/ProjetoImoveApplication.java
+++ projeto-imove/src/main/java/br/bandtec/com/projetoimove/ProjetoImoveApplication.java
@@ -2,11 +2,6 @@ package br.bandtec.com.projetoimove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.Query;
-import java.util.List;
-
-
-import java.util.Map;
 
 @SpringBootApplication
 public class ProjetoImoveApplication {
--- projeto-imove/src/test/java/br/bandtec/com/projetoimove/controller/UsuarioControllerTest.java
+++ projeto-imove/src/test/java/br/bandtec/com/projetoimove/controller/UsuarioControllerTest.java
@@ -2,6 +2,7 @@ package br.bandtec.com.projetoimove.controller;
 
 import br.bandtec.com.projetoimove.domains.Usuario;
 import br.bandtec.com.projetoimove.repository.UsuarioRepository;
+import br.bandtec.com.projetoimove.service.MailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +22,21 @@ class UsuarioControllerTest {
     UsuarioController controller;
 
     @MockBean
+    MailService mailService;
+
+    @MockBean
     UsuarioRepository repository;
+
+    @Test
+    void cadastrarUsuario() {
+        Usuario usuario = new Usuario();
+
+        when(repository.save(usuario)).thenReturn(usuario);
+
+        ResponseEntity resposta = controller.cadastrarUsuario(usuario);
+
+        assertEquals(201, resposta.getStatusCode().value());
+    }
 
     @Test
     void getUsuarios() {
@@ -30,9 +45,20 @@ class UsuarioControllerTest {
         when(repository.findAll()).thenReturn(new ArrayList<>());
         ResponseEntity resposta = controller.obterUsuarios();
 
-        assertEquals(usuario, resposta.getBody());
-        assertEquals(204, resposta.getStatusCode().equals(null));
-        assertEquals(200, resposta.getStatusCode().equals(usuario));
+        assertEquals(204, resposta.getStatusCode().value());
+    }
+
+    @Test
+    void mudarSenha() {
+        Usuario usuario = new Usuario();
+        usuario.setSenha("");
+
+      when(repository.findAll()).thenReturn(new ArrayList<>());
+      ResponseEntity resposta = controller.alterarSenha("teste101@gmail.com", "admin");
+
+      assertEquals(repository.save(usuario), resposta.getBody());
+      assertEquals(404, resposta.getStatusCode().value());
+
     }
 
 }
